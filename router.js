acbe8cae6a667c005f80771719c2378f587383a1
chi-white <king0209king0209@gmail.com>
2023-12-04 00:21:07 +0800

    refactor : change socket server position

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const handleSocketEvents = require('./socket/socketController');
+const handleSocketEvents = require('./doublegame/doublegameController');
 
 const app = express();
 const server = http.createServer(app);
